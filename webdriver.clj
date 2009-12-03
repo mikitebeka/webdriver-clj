efe7af0e99264d64ab5b557c0d44f3c48bb75e7f
Miki Tebeka <miki.tebeka@gmail.com>
2009-12-02 20:58:17 -0800

    Starting

---
+++ webdriver.clj
@@ -0,0 +1,72 @@
+; FIXME: This does not work
+; (import '(java.io File))
+; (add-classpath (str 
+;                  "file://" 
+;                  (.getCanonicalPath (new File ".")) 
+;                  "webdriver-all.jar"))
+(ns com.mikitebeka.webdriver
+  (:refer-clojure :exclude [get])
+  (:import [org.openqa.selenium By WebDriver WebElement]
+           [org.openqa.selenium.firefox FirefoxDriver]
+           [org.openqa.selenium.ie InternetExplorerDriver]))
+
+(def *drivers* {
+      :firefox FirefoxDriver
+      :ie InternetExplorerDriver })
+               
+(defn new-driver
+  "create new driver"
+  [browser]
+  (let [create
+        ({ :firefox (fn [] (new FirefoxDriver))
+          :ie (fn [] (new InternetExplorerDriver))} browser)]
+    (create)))
+
+(defn get
+  [driver url]
+  (.get driver url))
+
+(defn current-url
+  [driver]
+  (.getCurrentUrl driver))
+
+(defn title
+  [driver]
+  (.getTitle driver))
+
+; FIXME
+(defn find-elements
+  [driver]
+  [])
+
+; FIXME
+(defn find-element
+  [driver]
+  [])
+
+(defn page-source
+  [driver]
+  (.getPageSource driver))
+
+(defn close
+  [driver]
+  (.close driver))
+
+(defn quit
+  [driver]
+  (.quit driver))
+
+; FIXME
+(defn window-handles
+  [driver]
+  (into #{} (.getWindowHandles driver)))
+
+(defn window-handle
+  [driver]
+  (.getWindowHandle driver))
+
+(def driver (new-driver :firefox))
+(get driver "http://google.com")
+;(quit driver)
+
+;(System/exit 0)
